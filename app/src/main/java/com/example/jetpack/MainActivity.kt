ed3476ef654660e69986068d24d0851ee87b3782
Asta Bevainyte <asta.bevainyte@sky.uk>
2020-11-09 19:26:14 +0000

    AB: init working composose

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.Text
+import androidx.compose.ui.platform.setContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text("ASTA ")
+        }
     }
 }
